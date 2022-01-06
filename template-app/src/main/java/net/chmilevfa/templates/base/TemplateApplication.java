4d6432d297564b4d5a19da36d87906773b19a7a9
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2022-01-06 15:40:19 +0100

    add logger + simplest config

--- template-app/src/main/java/net/chmilevfa/templates/base/TemplateApplication.java
+++ template-app/src/main/java/net/chmilevfa/templates/base/TemplateApplication.java
@@ -1,8 +1,15 @@
 package net.chmilevfa.templates.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
 public class TemplateApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LOG.info("Hello world!");
     }
 }

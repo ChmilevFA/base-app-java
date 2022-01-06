9a1b7c5e83562ef1cf1a45b78bc332c02b0a02cf
chmilevfa <17650184+ChmilevFA@users.noreply.github.com>
2022-01-06 16:14:32 +0100

    add javalin

--- template-app/src/main/java/net/chmilevfa/templates/base/TemplateApplication.java
+++ template-app/src/main/java/net/chmilevfa/templates/base/TemplateApplication.java
@@ -1,15 +1,35 @@
 package net.chmilevfa.templates.base;
 
+import io.javalin.Javalin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
+import static java.lang.Runtime.getRuntime;
+
 public class TemplateApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private static Javalin server;
+
     public static void main(String[] args) {
-        LOG.info("Hello world!");
+        LOG.info("Starting the application...");
+
+        server = Javalin.create().start(7000);
+        server.get("/", ctx -> ctx.result("Hello World!"));
+
+        registerShutdownHook();
+
+        LOG.info("Application successfully started");
+
+    }
+
+    private static void registerShutdownHook() {
+        getRuntime().addShutdownHook(new Thread(() -> {
+            LOG.info("Stopping the application");
+            server.close();
+        }));
     }
 }
